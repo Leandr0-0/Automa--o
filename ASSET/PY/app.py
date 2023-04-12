9c10fc6e0f7e3084d89f37e97392f0d028281930
Leandro Silva <sr.syllw@gmail.com>
2023-04-12 14:36:58 -0300

    Descrição do Programa

---
+++ ASSET/PY/app.py
@@ -0,0 +1,7 @@
+''' 
+Tenho que entrar no site https://cadastroprodutos-devaprender.netlify.app/, após entrar no site,
+eu abro a planilha,  após te raberto a planilha eu devo copiar o dado que está dentro do campo PRODUTO 
+e colar no seu campo respectivo dentro do Sistema Web. Copiar o dado que estar dentro do campo FORNECEDOR 
+e colar dentro de seu campo respectivo. Copiar dados que estar dentro do campo CATEGORIA e (...) 
+Ao finalizar a primeira estrada, ir para a próxima até finalizar a entrada de todos os dados.
+'''
