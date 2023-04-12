50676c3b6d4dd1a981d4208bd2e4267a97d66dd1
Leandro Silva <sr.syllw@gmail.com>
2023-04-12 15:37:07 -0300

    Instalações de Bots

--- ASSET/PY/app.py
+++ ASSET/PY/app.py
@@ -1,7 +1,11 @@
 ''' 
-Tenho que entrar no site https://cadastroprodutos-devaprender.netlify.app/, após entrar no site,
-eu abro a planilha,  após te raberto a planilha eu devo copiar o dado que está dentro do campo PRODUTO 
-e colar no seu campo respectivo dentro do Sistema Web. Copiar o dado que estar dentro do campo FORNECEDOR 
-e colar dentro de seu campo respectivo. Copiar dados que estar dentro do campo CATEGORIA e (...) 
-Ao finalizar a primeira estrada, ir para a próxima até finalizar a entrada de todos os dados.
+Tenho que entrar no site https://cadastroprodutos-devaprender.netlify.app/, após entrar no site,eu abro a planilha,  após te aberto a planilha eu devo copiar os dados que está dentro do campo PRODUTO e colar no seu campo respectivo dentro do Sistema Web. Copiar o dado que estar dentro do campo FORNECEDOR e colar dentro de seu campo respectivo. Copiar dados que estar dentro do campo CATEGORIA e (...) Ao finalizar a primeira estrada, ir para a próxima até finalizar a entrada de todos os dados.
 '''
+
+# 1 - Acessar o site https://cadastroprodutos-devaprender.netlify.app/;
+# 2 - Abrir a planilha;
+# 3 - Copiar os dados que está dentro do campo PRODUTO e colar no seu campo respectivo dentro do Sistema Web;
+# 4 - Repetir o mesmo processo para as outras colunas;
+# 5 - Repetir até chegar ao último cadastro da planilha com os 500 cadastros.
+
+
