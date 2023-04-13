49f3bccd48a3c862ffc917e42109614241dd7df9
Leandro Silva <sr.syllw@gmail.com>
2023-04-13 14:04:43 -0300

    Classificar

--- ASSET/PY/app.py
+++ ASSET/PY/app.py
@@ -6,6 +6,4 @@ Tenho que entrar no site https://cadastroprodutos-devaprender.netlify.app/, apó
 # 2 - Abrir a planilha;
 # 3 - Copiar os dados que está dentro do campo PRODUTO e colar no seu campo respectivo dentro do Sistema Web;
 # 4 - Repetir o mesmo processo para as outras colunas;
-# 5 - Repetir até chegar ao último cadastro da planilha com os 500 cadastros.
-
-
+# 5 - Repetir até chegar ao útimo cadastro da planilha com os 500 cadastro.
