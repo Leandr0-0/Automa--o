b9c750e5afc059177bc793749aa6395be34cf0d8
Leandro Silva <sr.syllw@gmail.com>
2023-04-13 14:43:42 -0300

    Moldando

--- ASSET/PY/app.py
+++ ASSET/PY/app.py
@@ -1,3 +1,4 @@
+import pyautogui
 ''' 
 Tenho que entrar no site https://cadastroprodutos-devaprender.netlify.app/, após entrar no site,eu abro a planilha,  após te aberto a planilha eu devo copiar os dados que está dentro do campo PRODUTO e colar no seu campo respectivo dentro do Sistema Web. Copiar o dado que estar dentro do campo FORNECEDOR e colar dentro de seu campo respectivo. Copiar dados que estar dentro do campo CATEGORIA e (...) Ao finalizar a primeira estrada, ir para a próxima até finalizar a entrada de todos os dados.
 '''
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Automação de Planilha
-Automação de produtos de uma planilha para um sistema online com o intuito de otimizar o trabalho dos vendedores.
+Automação de produtos de uma planilha para um sistema online com o intuito de otimizar o trabalho dos vendedores. Cada pedaço da planilha será automotizado.
